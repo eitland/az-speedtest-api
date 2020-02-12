472d91ad0590ce273ed4dd9279685ee6d95308cf
Erik Vårdal Itland <erik.vardal.itland@computas.com>
2020-02-12 15:03:16 +0100

    Add POST handling

--- SpeedTestApi/Controllers/SpeedTestController.cs
+++ SpeedTestApi/Controllers/SpeedTestController.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SpeedTestApi.Models;
 
 namespace SpeedTestApi.Controllers {
     [ApiController]
     [Route ("[controller]")]
     public class SpeedTestController : ControllerBase {
+
+        private readonly ILogger _logger;
+
+        public SpeedTestController (ILogger<SpeedTestController> logger) {
+            _logger = logger;
+        }
 
         // GET speedtest/ping
         [Route ("ping")]
@@ -16,5 +19,15 @@ namespace SpeedTestApi.Controllers {
         public string Ping () {
             return "PONG";
         }
+
+        // POST speedtest/
+        [HttpPost]
+        public string UploadSpeedTest ([FromBody] TestResult speedTest) {
+            var response = $"Got a TestResult from { speedTest.User } with download { speedTest.Data.Speeds.Download } Mbps.";
+            _logger.LogInformation (response);
+
+            return response;
+        }
+
     }
 }
